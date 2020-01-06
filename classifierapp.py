261d0749c7c6fbc91b2ca4c6da24639de947b76a
hmuus01 <hamzemuuse1@gmail.com>
2020-01-06 21:50:04 +0000

    playing with the random seed

--- classifierapp.py
+++ classifierapp.py
@@ -13,7 +13,7 @@ import pickle
 
 import matplotlib.pyplot as plt
 
-#
+
 proba_threshold = 0.2
 
 #Array to store the accuracies and the recalls
@@ -33,7 +33,7 @@ numberOfOnes = credit_data_df_fraud.shape[0]
 load_balancing_ratio = 1.0
 numberOfZeros = math.floor(load_balancing_ratio * numberOfOnes)
 
-random_seeds = [12] #, 23, 34, 1, 56, 67, 45, 6]
+random_seeds = [12, 23, 34, 1, 56, 67, 45, 6]
 
 #Method to plot the ROC curve
 def plot_roc():
